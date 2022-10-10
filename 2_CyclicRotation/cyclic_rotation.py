496b9174e563c372eb1a19b8bcde93e44c1fc5d1
kingsleyocran <ocrankingsley@gmail.com>
2022-10-10 11:56:06 +0000

    add odd occurence

--- 2_CyclicRotation/cyclic_rotation.py
+++ 2_CyclicRotation/cyclic_rotation.py
@@ -4,21 +4,17 @@ from array import array
 def solution(A: array, K: int) -> array:
     # Edge cases
     # if len(A) == 0
-    if (len(A)/K == 0):
-        return A
-
-    # if a rotation = the len of arr
     if (len(A) == 0):
         return A
     
     # if all elements are the same
-    if len(set(A)) == 1:
-        return A
+    if (len(A) > 0):
+        if len(set(A)) == 1:
+            return A
 
     # if K = 0  
     if (K == 0):
         return A
-
 
     K = K % len(A)
     return A[-K:] + A[:-K]
