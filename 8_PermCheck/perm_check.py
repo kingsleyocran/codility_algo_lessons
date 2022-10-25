446abbf2e340cf46b6a5b0fc420538be0cb7ab92
kingsleyocran <ocrankingsley@gmail.com>
2022-10-25 23:11:04 +0000

    add new files

--- 8_PermCheck/perm_check.py
+++ 8_PermCheck/perm_check.py
@@ -1,1 +1,14 @@
 
+# Same as Perm Missing Element except that you are just return 0 or 1 as checks.
+def solution(A):
+    # Edge case
+    if (len(A) == 0):
+        return 1
+
+    A.sort()
+
+    for i in range(0, len(A)):
+        if A[i] != i + 1:
+            return 0
+    
+    return 1
