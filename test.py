c54d064b89fe0c26e4192bbcf56a6d52510e9196
kingsleyocran <ocrankingsley@gmail.com>
2022-10-28 15:35:39 +0000

    add evaluate test

--- test.py
+++ test.py
@@ -6,30 +6,18 @@ now = datetime.datetime.now()
 
 print(f'{now:%Y-%m-%d %H:%M}')
 
-# Python Program to Update
-# character of a String
+# Parent Class
+class A(object):
+    def __init__(self, n='Rahul'):
+        self.name = n
  
-String1 = "Hello, I'm a Geek"
-print("Initial String: ")
-print(String1)
  
-# Updating a character of the String
-## As python strings are immutable, they don't support item updation directly
-### there are following two ways
-#1
-list1 = list(String1)
-list1[2]
-String2 = ''.join(list1)
-print("\nUpdating character at 2nd Index: ")
-print(String2)
+class B(A):
+    def __init__(self, roll):
+        self.roll = roll
 
-list2 = list(String2)
-list2.pop(2);
-String7 = ''.join(list2)
-print(String)
-
-
-
-#2
-String3 = String1[0:2] + 'p' + String1[3:]
-print(String3)
+        A.__init__(self)
+ 
+ 
+object = B(23)
+print(object.name)
